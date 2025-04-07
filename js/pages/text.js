8db7470fd177fc3a5a822248eec4ceba5787fef8
Rokas <Rokas.Imelinskas@gmail.com>
2025-04-07 14:39:23 +0300

    DOM

---
+++ js/pages/text.js
@@ -0,0 +1,45 @@
+const titleDOM = document.getElementById("title");
+console.log(titleDOM);
+console.log(titleDOM.textContent);
+console.log(titleDOM.innerText);
+console.log(titleDOM.textContent.length);
+console.log(titleDOM.innerText.length);
+
+titleDOM.textContent = "Pomidoras";
+titleDOM.innerText = "Agurkas";
+
+const liDOM = document.querySelectorAll("ul li");
+console.log(liDOM);
+
+for (let i = 0; i < liDOM.length; i++) {
+  liDOM[i].textContent = liDOM[i].textContent.repeat(5) + "!";
+}
+
+for (const li of liDOM) {
+  li.textContent = li.textContent.repeat(3) + "?";
+}
+
+const colors = ["yellow", "green", "red"];
+const vegsDOM = document.querySelectorAll("ol li");
+console.log(vegsDOM);
+
+let fontSize = 1;
+let index = 0;
+
+for (const vegDOM of vegsDOM) {
+  //   const colorIndex = Math.floor(Math.random() * colors.length);
+  const colorIndex = index++ % colors.length;
+  const color = colors[colorIndex];
+  vegDOM.style.fontSize = 1 + index * 0.3 + "rem";
+  vegDOM.style.backgroundColor = color;
+  fontSize += 0.3;
+}
+
+const lrlDOM = document.querySelectorAll("div p");
+for (const pDOM of lrlDOM) {
+  const { number, color } = pDOM.dataset;
+  pDOM.textContent = number + ") " + pDOM.textContent;
+  pDOM.style.backgroundColor = color;
+  console.log(pDOM.dataset);
+  console.log(pDOM.dataset.color);
+}
