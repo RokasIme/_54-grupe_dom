2e058ac898041b3d8c2119d59e1026eded019eeb
Rokas <Rokas.Imelinskas@gmail.com>
2025-04-09 14:51:38 +0300

    click

---
+++ js/components/header.js
@@ -0,0 +1,33 @@
+export function header() {
+  // const body = document.querySelector("body");
+  //   const body = document.body;
+  const menu = [
+    { text: "Home", href: "/" },
+    { text: "Text", href: "/text" },
+    { text: "Food", href: "/food" },
+    { text: "Darzas", href: "/darzas" },
+    { text: "Header", href: "/header" },
+    { text: "Click", href: "/click" },
+  ];
+
+  const currentPage =
+    location.pathname.length > 1 && location.pathname.at(-1) === "/"
+      ? location.pathname.slice(0, -1)
+      : location.pathname;
+  let linksHTML = "";
+
+  for (const link of menu) {
+    let activePage = "";
+    if (link.href === currentPage) {
+      activePage = "active";
+    }
+    linksHTML += `<a class="link ${activePage}" href="${link.href}">${link.text}</a> `;
+  }
+  const HTML = `
+    <header class="main-header">
+      <img class="logo" src="../food/pizza.webp" alt="Logo" />
+      <nav class="main-nav">${linksHTML}</nav>
+    </header>
+    `;
+  document.body.insertAdjacentHTML("afterbegin", HTML);
+}
--- js/pages/food.js
+++ js/pages/food.js
@@ -1,5 +1,8 @@
 import { foodList } from "../data/pizza.js";
 import { foodBlock } from "../components/foodBlock.js";
+import { header } from "../components/header.js";
+
+header();
 
 const currency = "Eur";
 
