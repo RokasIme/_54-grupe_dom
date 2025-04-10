aca3d4f521c75dd77e8f8bf097354d7b39cc4cde
Rokas <Rokas.Imelinskas@gmail.com>
2025-04-10 13:15:12 +0300

    links

--- js/components/foodBlock.js
+++ js/components/foodBlock.js
@@ -1,7 +1,7 @@
 export function foodBlock(item, currency) {
   return `
     <li class="food">
-        <img class="img" src="./${item.photo}" alt="Pizza" />
+        <img class="img" src="./food/${item.photo}" alt="Pizza" />
         <p class="title">${item.name}</p>
         <p class="price">${item.price / 100} ${currency}}</p>
         <p class="availability"><span>${item.availability}</span> available</p>
--- js/components/header.js
+++ js/components/header.js
@@ -1,56 +1,42 @@
 export function header() {
+  let base = "http://localhost:5416/";
+  let projectName = "";
+
+  if (location.hostname !== "localhost") {
+    projectName = "/_54-grupe_dom";
+    // projectName = '/' + location.pathname.split('/')[1];
+    base = "https://github.com/RokasIme/_54-grupe_dom/";
+    // base = location.origin + projectName
+  }
+  document.head.insertAdjacentHTML("afterbegin", `<base href ="${base}">`);
+
   // const body = document.querySelector("body");
   //   const body = document.body;
   const menu = [
     { text: "Home", href: "/" },
-    { text: "Text", href: "/text" },
-    { text: "Food", href: "/food" },
-    { text: "Darzas", href: "/darzas" },
-    { text: "Header", href: "/header" },
-    { text: "Click", href: "/click" },
+    { text: "Text", href: "/text/" },
+    { text: "Food", href: "/food/" },
+    { text: "Darzas", href: "/darzas/" },
+    { text: "Header", href: "/header/" },
+    { text: "Click", href: "/click/" },
   ];
 
-  const currentPage =
-    location.pathname.length > 1 && location.pathname.at(-1) === "/"
-      ? location.pathname.slice(0, -1)
-      : location.pathname;
   let linksHTML = "";
 
   for (const link of menu) {
     let activePage = "";
-    if (link.href === currentPage) {
+    if (projectName + link.href === location.pathname) {
       activePage = "active";
     }
-    // pathname: "/_54-grupe_dom/food/"
-    if (
-      link.href === currentPage.slice(14) ||
-      currentPage === "/_54-grupe_dom"
-    ) {
-      activePage = "active";
-    }
-
-    if (location.pathname === "/_54-grupe_dom/") {
-      linksHTML += `<a class="link ${activePage}" href=".${link.href}">${link.text}</a> `;
-    } else {
-      linksHTML += `<a class="link ${activePage}" href="..${link.href}">${link.text}</a> `;
-    }
+    linksHTML += `<a class="link ${activePage}" href=".${link.href}">${link.text}</a> `;
   }
 
   let HTML = `
     <header class="main-header">
-      <img class="logo" src="./food/pizza.webp" alt="Logo" />
+      <img class="logo" src="./img/logo.webp" alt="Logo" />
       <nav class="main-nav">${linksHTML}</nav>
     </header>
     `;
-
-  if (location.pathname !== "/_54-grupe_dom/") {
-    HTML = `
-    <header class="main-header">
-      <img class="logo" src="../food/pizza.webp" alt="Logo" />
-      <nav class="main-nav">${linksHTML}</nav>
-    </header>
-    `;
-  }
 
   document.body.insertAdjacentHTML("afterbegin", HTML);
 }
